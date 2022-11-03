d07171d207b0b89adc84ccd9f4f1042c3f48b896
Aleh Barushka <1.alehbarushka@gmail.com>
2022-11-03 18:27:21 +0300

    init CRA

---
+++ bot/index.js
@@ -0,0 +1,21 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = '5438133696:AAHKJ7pLWrC-6Z2RQlmbC9RSm3CADvCO5UY';
+const WEB_APP_URL = 'https://github.com/AlehBarushka';
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on('message', async (mes) => {
+  const text = mes.text;
+  const chatId = mes.chat.id;
+
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'Даров!', {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Заполнить форму', web_app: { url: WEB_APP_URL } }],
+        ],
+      },
+    });
+  }
+});
